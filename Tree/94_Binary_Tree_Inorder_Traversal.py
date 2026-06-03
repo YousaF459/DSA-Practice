6334b23f313023e31d16cc7d44430b0fb55df992
Yousaf <khanyousafamin@gmail.com>
2026-06-03 19:55:16 +0500

    Tree Problems Solved

---
+++ Tree/94_Binary_Tree_Inorder_Traversal.py
@@ -0,0 +1,27 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def inorderTraversal(self, root) :
+        if not root:
+            return []
+        stack=[]
+        res=[]
+
+        currentNode=root
+        while currentNode or stack:
+
+            while currentNode:
+                stack.append(currentNode)
+                currentNode=currentNode.left
+
+            
+            currentNode=stack.pop()
+            res.append(currentNode.val)
+            currentNode=currentNode.right
+        
+        return res

1e5afadce8bdd70aa7a84360369d14b6487aff13
Yousaf <khanyousafamin@gmail.com>
2026-06-19 17:47:29 +0500

    Recursion Subset Solved

--- Recursion/78_Subsets.py
+++ Recursion/78_Subsets.py
@@ -1,16 +1,21 @@
 class Solution:
-    def subsets(self, nums: List[int]) -> List[List[int]]:
+    def subsets(self, nums) :
         result=[]
-
         
+        def backtrack(start,current):
+            result.append(current[:])
 
+            for i in range(start,len(nums)):
 
-
-
+                current.append(nums[i])
+                backtrack(i+1,current)
+                current.pop()
+        
+        backtrack(0,[])
         return result
     
 
 sol=Solution()
 
 nums = [1,2,3]
-print(sol.subsets(0))
+print(sol.subsets(nums))
